193f645400f160eff147593f3282cb31cf38f585
soldat7414 <soldat7414@gmail.com>
2022-04-02 23:28:57 +0300

    Add class App (App launcher) with methods for initial variables, transfer data to processing and output data

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,45 @@
 package app;
 
 public class App {
+
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+    static String roundBonus;
+    static ProductA product;
+    static MerchantA merchantA;
+    static String infoMerchant;
+    static String infoProduct;
+
+
     public static void main(String[] args) {
-        System.out.println("Initial commit");
+        initVars();
+        showData(processData());
+    }
+
+    private static void initVars(){
+        merchantName = "Роберт";
+        email = "bob@mail.com";
+        phone = "055 123-4567";
+        productName = "апельсины";
+        quantity = 1000;
+        price = 19.99;
+    }
+
+    private static String processData(){
+        merchantA = new MerchantA(merchantName, phone, email);
+        infoMerchant = merchantA.infoMerchant();
+        product = new ProductA(productName, quantity, price);
+        infoProduct = product.infoProduct();
+        double sales = product.calcSales(quantity, price);
+        roundBonus = Rounder.roundValue(merchantA.calcBonus(sales));
+        return infoMerchant + infoProduct + "\nБонус (грн.): " + roundBonus;
+    }
+
+    private static void showData(String output){
+        System.out.println(output);
     }
 }
