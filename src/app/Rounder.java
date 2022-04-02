13cd33aeb53ed75e8ec3291b46947b6fd9d966ef
soldat7414 <soldat7414@gmail.com>
2022-04-02 22:40:09 +0300

    Add class Rounder whose performs only one task - rounds to two decimal places

---
+++ src/app/Rounder.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.text.DecimalFormat;
+
+public class Rounder {
+    public static String roundValue(double value){
+        DecimalFormat df = new DecimalFormat("0.00");
+        return df.format(value);
+//        return new DecimalFormat("0.00").format(value);
+    }
+}
